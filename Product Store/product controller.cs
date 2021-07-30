ef0b22e57b60e500dd70ff4e6d5e276eed4073ed
Naren0225 <narendranaren881@gmail.com>
2021-07-30 14:40:25 +0530

    Add project files.

---
+++ Product Store/product controller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product_Store
+{
+    class product_controller
+    {
+        private List<product> products;
+
+        public product_controller()
+        {
+            products = new List<product>();
+
+        }
+
+        public void addItem(product product)
+        {
+            products.Add(product);
+
+        }
+        public void display()
+        {
+            
+            foreach (product p in products)
+            {
+                Console.WriteLine("-----------");
+
+                Console.WriteLine("id :" + p.id);
+                Console.WriteLine("name :" + p.name);
+                Console.WriteLine("price :" + p.price);
+                Console.WriteLine("color :" + p.color);
+                Console.WriteLine("model :" + p.model);
+
+                Console.WriteLine("-----------");
+            }
+
+        }
+        
+    }
+
+}
+
+    
+
+
---
+++ Product Store/product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product_Store
+{
+    class product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+        public string color { get; set; }
+        public int model { get; set; }
+    }
+}
